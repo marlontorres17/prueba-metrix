44473f5700fd6591b3ecf523b509aa8e77b60e0b
marlontorres17 <marlontorresmedina@gmail.com>
2025-04-25 21:17:56 -0500

    feat(backend): Implementar ApiResponse

--- contacto/src/main/java/com/metrix/contacto/contacto/infrastructure/controller/ContactController.java
+++ contacto/src/main/java/com/metrix/contacto/contacto/infrastructure/controller/ContactController.java
@@ -2,45 +2,38 @@ package com.metrix.contacto.contacto.infrastructure.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.metrix.contacto.common.application.dto.ApiResponse;
+import com.metrix.contacto.common.infrastructure.codes.ApiCode;
 import com.metrix.contacto.contacto.application.command.CreateContactCommand;
 import com.metrix.contacto.contacto.application.useCase.CreateContactUseCase;
 import com.metrix.contacto.contacto.application.useCase.GetDailySubmissionsUseCase;
 
 @RestController
-@RequestMapping("/api/contact-submissions")
+@RequestMapping
 @RequiredArgsConstructor
 public class ContactController {
 
     private final CreateContactUseCase createContactUseCase;
     private final GetDailySubmissionsUseCase getDailySubmissionsUseCase;
 
-    @PostMapping
-    public ResponseEntity<?> submitContactForm(@Valid @RequestBody CreateContactCommand command) {
+    @PostMapping("/api/contact-submissions")
+    public ResponseEntity<ApiResponse<Object>> submitContactForm(@Valid @RequestBody CreateContactCommand command) {
         createContactUseCase.execute(command);
-        return new ResponseEntity<>(new ApiResponse("Formulario recibido correctamente."), HttpStatus.CREATED);
+        return ResponseEntity
+                .status(ApiCode.CREATED.getHttpStatus())
+                .body(ApiResponse.success(null, "Formulario enviado exitosamente", ApiCode.CREATED.getCode()));
     }
 
-    // Clase de respuesta en español
-    private static class ApiResponse {
-        private final String mensaje;
-
-        public ApiResponse(String mensaje) {
-            this.mensaje = mensaje;
-        }
-
-        public String getMensaje() {
-            return mensaje;
-        }
-    }
-
-    @GetMapping("/daily-submissions")
-    public ResponseEntity<Object> getDailySubmissions() {
+    @GetMapping("/api/metrics/daily-submissions")
+    public ResponseEntity<ApiResponse<Long>> getDailySubmissions() {
         long dailySubmissions = getDailySubmissionsUseCase.execute();
-        return ResponseEntity.ok().body("{\"conteo\": " + dailySubmissions + "}");
+        return ResponseEntity
+                .status(ApiCode.OK.getHttpStatus())
+                .body(ApiResponse.success(dailySubmissions, "Consulta de formularios diarios exitosa",
+                        ApiCode.OK.getCode()));
     }
+
 }
